89761aaa49fcf3cb23e0b6f462839aa80c56de99
tejal_deshpande <tejal.deshpande78@outlook.com>
2022-09-13 14:27:47 +0530

    views changed to generic views

--- blogs/views.py
+++ blogs/views.py
@@ -1,28 +1,47 @@
 from django.shortcuts import render
 from django.views import *
 from blogs.models import *
+from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
 
 # view all blogs
-class ViewAll(View):
-    def get(self, request):
-        bl = BlogPosts.objects.all()
-        context = {
-            "data": bl,
-        }
-        return render(request, "blogs/allblogs.html", context)
+class ViewAll(ListView):
+    model = BlogPosts
+    template_name = "blogs/allblogs.html"
 
 
 # view 1 blog based on id from query param
-class ViewOne(View):
-    def get(self, request):
-        ind = request.GET.get("id")
-        if ind:
-            bl = BlogPosts.objects.get(id=ind)
-            context = {
-                "data": bl,
-            }
-        else:
-            context = {
-                "data": None,
-            }
-        return render(request, "blogs/oneblog.html", context)
+# class ViewOne(View):
+#     def get(self, request):
+#         ind = request.GET.get("id")
+#         if ind:
+#             bl = BlogPosts.objects.get(id=ind)
+#             context = {
+#                 "data": bl,
+#             }
+#         else:
+#             context = {
+#                 "data": None,
+#             }
+#         return render(request, "blogs/oneblog.html", context)
+
+
+class ViewOne(DetailView):
+    model = BlogPosts
+    template_name = "blogs/oneblog.html"
+
+    # override queryset func
+    # this doesnt work for url parameters
+    # it works only for urls.p hardcoded id
+    # def get_queryset(self):
+    #     ind = self.request.GET.get("id")
+    #     print(ind)
+    #     bl = BlogPosts.objects.get(id=ind)
+    #     return bl
+
+    # overriding get_object function instead
+    def get_object(self):
+        ind = self.request.GET.get("id")
+        print(ind)
+        bl = BlogPosts.objects.get(id=ind)
+        return bl
